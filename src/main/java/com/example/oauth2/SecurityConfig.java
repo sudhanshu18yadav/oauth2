2a49142b4fadd16db82011faf70f247e665e47c6
yadavsudhanshu1824@gmail.com <yadavsudhanshu1824@gmail.com>
2025-05-13 11:03:45 +0530

    start for next day

--- src/main/java/com/example/oauth2/SecurityConfig.java
+++ src/main/java/com/example/oauth2/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,11 +18,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll())
-                .formLogin(Customizer.withDefaults());
-        
+        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());
+        // .oauth2ResourceServer((oauth2)-> oauth2.jwt(Customizer.withDefaults()));
+
+        var test = JwtDecoders.fromIssuerLocation("http://localhost:4200");
+        System.out.println("---"+ test);
         return http.build();
     }
+
+    // @Bean
+    // public JwtDecoder jwtDecoder(){
+    // return JwtDecoders.fromIssuerLocation("http://localhost:4200");
+    // }
 
     @Bean
     public UserDetailsService userDetailsService() {
